880320337e7ea10dfa428f35844290e408c27840
harsh12met <harshalmali11204@gmail.com>
2025-09-26 11:26:16 +0530

    Fix Angular routing and add redirects for Netlify 404 issues

--- my-kanban-project/src/app/app.routes.ts
+++ my-kanban-project/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BoardComponent },
+  { path: '**', redirectTo: '' }
+];
